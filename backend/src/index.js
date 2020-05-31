be36ff627a95fc4a511f5f23b424d8928c40e355
Matheus <manomanmatec@gmail.com>
2020-05-31 12:36:20 -0300

    Corrigindo a configuracao do heroku

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
