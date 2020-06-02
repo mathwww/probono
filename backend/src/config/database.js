6eafe92ee4c9aa63c0ada0ce095011654cd236b0
Matheus <manomanmatec@gmail.com>
2020-06-02 20:53:54 -0300

    Adicionando um banco Postgres em Docker e integrando-o com a aplicacao

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    database: 'probono',
+    username: 'postgres',
+    password: 'probono',
+    port: 5432,
+    define: {
+        timestamps: true
+    },
+};
