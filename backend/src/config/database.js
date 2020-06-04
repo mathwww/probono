4eafa4107961c770f36bb630614cf8a8f6edaa37
Matheus <manomanmatec@gmail.com>
2020-06-04 01:41:37 -0300

    Adicionando ambientes distintos a config do banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'probono',
+    database: 'probono',
+    host: 'localhost',
+    dialect: 'postgres'
+  },
+  production: {
+    username: 'spexsorxokqacz',
+    password: '15f7dc77361f048850fb0b48df440c4ec826a7f25e4a6d9bab19d31e673b1648',
+    database: 'dc6m2sqkfgsit6',
+    host: 'ec2-3-222-150-253.compute-1.amazonaws.com',
+    dialect: 'postgres',
+  }
+};
+
+/*
 module.exports = { 
     url: process.env.DATABASE_URL,
     dialect: 'postgres'
@@ -12,3 +32,4 @@ module.exports = {
         timestamps: true
     },
 };
+*/
