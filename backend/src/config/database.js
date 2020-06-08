1d26397ecb81481dd66fb1c57e878c55b588d1f2
Matheus <manomanmatec@gmail.com>
2020-06-08 00:10:50 -0300

    Corrigindo conexao com instancia do banco em deploy

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    database: 'probono',
-    username: 'postgres',
-    password: 'probono',
+    host: process.env.DATABASE_HOST || 'localhost',
+    database: process.env.DATABASE_NAME || 'probono',
+    username: process.env.DATABASE_USER || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'probono',
     port: 5432,
     define: {
         timestamps: true
