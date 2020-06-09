954e1ecf64771e9042adf6a6ac9f1285af496b5d
Matheus <manomanmatec@gmail.com>
2020-06-08 22:54:36 -0300

    Nova tentativa

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = process.env.API_URL || 'http://localhost:3333';
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3333';
 
 const api = axios.create({
     baseURL: apiUrl,
